99aa9ea43a6c925414539b8a7dceac49b08cf555
Andy Mention <amention@gmail.com>
2017-02-19 14:22:40 -0700

    Add integration test for aggregation
    * This test expects to be able to access a RabbitMQ instance. The
    easiest way to do that will be running `docker-compose up` in a separate
    shell. No CI for now :(
    * The module APIs are still fluid while figuring things out, and a
    high-level test doesn't have to take that into account. I mostly want to
    be able to do some quick verification that the overall project works
    while developing.
    * Unit tests should come (along with edge cases) as this project takes a
    little more form.

--- test/agg_mqp_test.exs
+++ test/agg_mqp_test.exs
@@ -1,8 +1,37 @@
 defmodule AggMqpTest do
   use ExUnit.Case
-  doctest AggMqp
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "it can aggregate two messages" do
+    #General Setup
+    {:ok, conn} = AMQP.Connection.open
+    {:ok, chan} = AMQP.Channel.open(conn)
+    AMQP.Exchange.declare chan, "test_exchange", :topic
+
+    #params
+    route1 = "routing.key1"
+    route2 = "routing.key2"
+    route3 = "routing.key3"
+    exchange = "test_exchange"
+
+    connection = %Connection{chan: chan, exchange: exchange}
+    aggregation = %Aggregation{
+      subscribe_routing_keys: [route1, route2],
+      publish_routing_key: "publish_queue"
+    }
+
+    Server.start_link(connection, aggregation)
+
+    # prepare publish queue
+    AMQP.Queue.declare(chan, "publish_queue")
+    AMQP.Queue.bind(chan, "publish_queue", "test_exchange", [routing_key: "publish_queue"])
+
+    AMQP.Basic.publish chan, "test_exchange", route1, "Hello, World!"
+    AMQP.Basic.publish chan, "test_exchange", route2, "Hello, World!"
+
+    #Give a moment to aggregate and publish
+    :timer.sleep 10
+
+    {:ok, payload, meta}  = AMQP.Basic.get chan, "publish_queue"
+    assert payload == "Hello, World!Hello, World!"
   end
 end
